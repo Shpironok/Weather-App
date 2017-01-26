f9608eb4a8e25e97b3be407b461b4580ed0036c6
Shpironok <shpironoka@gmail.com>
2017-01-26 11:10:59 +0300

    Final commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,6 @@ export default myapp => {
 // контроллер для приложения
 
 myapp.controller('WeatherCtrl', function ($scope, weatherService, citiesService) {
-    $scope.weather = weatherService.getWeather('Самара', '6'); // Выводим погоду для города по умолчанию
     $scope.countdays = [
       {count:'1'},
       {count:'2'},
@@ -14,20 +13,21 @@ myapp.controller('WeatherCtrl', function ($scope, weatherService, citiesService)
       {count:'7'},
     ];
     $scope.cnt = $scope.countdays[5];
+    $scope.weather = weatherService.getWeather('Samara', $scope.cnt); // Выводим погоду для города по умолчанию
 
     $scope.load = function (){ //обрабатываем клик по кнопке "Показать"
 
         if (!$scope.city) {
           $scope.bcolor = {'border-color':'red'};
         } else {
-        	$scope.weather = weatherService.getWeather($scope.city, $scope.cnt);
+          $scope.weather = weatherService.getWeather($scope.city, $scope.cnt);
           $scope.bcolor = {'border-color':'#cccccc'};
         }
     }
 
     $scope.cities = function (){ // Обрабатываем изменение текстового поля
         if ($scope.city.length > 3 ) {
-        	$scope.citytitle = citiesService.getCities($scope.city);
+          $scope.citytitle = citiesService.getCities($scope.city);
         }
     }
 });
